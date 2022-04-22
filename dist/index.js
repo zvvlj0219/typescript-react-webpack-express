97705c3400e976710381dbf625e2db1398ee9a78
zvvlj0219 <s00302448@gmail.com>
2022-04-23 03:05:10 +0900

    wildcard

--- dist/index.js
+++ dist/index.js
@@ -3,16 +3,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
-const path_1 = __importDefault(require("path"));
-const app = (0, express_1.default)();
+var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var path_1 = __importDefault(require("path"));
+var app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cors_1.default)());
-app.post('/api/eh', (req, res) => {
-    const value = req.body.text;
-    const valueArr = value.split('');
+app.post('/api/eh', function (req, res) {
+    var value = req.body.text;
+    var valueArr = value.split('');
     if (valueArr.indexOf('r') !== -1 ||
         valueArr.indexOf('e') !== -1 ||
         valueArr.indexOf('a') !== -1 ||
@@ -27,12 +27,12 @@ if (process.env.NODE_ENV === 'production') {
     // static folder
     app.use(express_1.default.static(path_1.default.resolve(__dirname)));
     // handle spa
-    app.get('/', (req, res) => {
+    app.get('*', function (req, res) {
         res.sendFile(path_1.default.resolve(__dirname, './index.html'));
     });
 }
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`NODE_ENV is ${process.env.NODE_ENV}`);
-    console.log(`server running port 5000 at http://localhost:${port}`);
+var port = process.env.PORT || 5000;
+app.listen(port, function () {
+    console.log("NODE_ENV is ".concat(process.env.NODE_ENV));
+    console.log("server running port 5000 at http://localhost:".concat(port));
 });
